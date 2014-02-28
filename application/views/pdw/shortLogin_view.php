2c20229bccc9ff27f1132e85ff5cc4f522f45ec4
Rahul Sonar <sonar.soni@gmail.com>
2014-02-28 11:12:15 +0530

    shortlogin flow

---
+++ application/views/pdw/shortLogin_view.php
@@ -0,0 +1,48 @@
+<script type="text/javascript">
+;(function($) { 
+$(document).ready(function() {
+	$("#frmShortLogin").validate({
+		rules: {
+			signupEmail: {
+				required: true
+			}
+		},errorElement: "div",
+		messages: {
+		},
+		errorPlacement: function(error, element) {
+			error.insertAfter(element);
+		},
+		submitHandler: function(){
+			xajax_shortLoginSubmit(xajax.getFormValues('frmShortLogin'));
+		}
+		
+	});	
+})
+})(jQuery); 
+</script>
+<div style="display:none;">
+  <div id="shortLogin"> 
+        <div class="hearderholder">LOGIN</div>
+        <div class="lightboxcontent">
+            <div class="lightboxcontentleft">
+                <!-- <h2>Registered Customer</h2>
+                <p>If you already have an account please log in.</p> -->
+
+                <form name="frmShortLogin" id="frmShortLogin" method="post">
+                    <div class="frmholder">
+                        <label>Email Address / Mobile Number</label>
+                        <input type="text" name="signupEmail" id="signupEmail" class="inplog" />
+                    </div>
+                    <div class="btnholder">
+                        <input type="submit" style="float:left;" class="btnprced" value="PROCEED"  />
+                        <a href="#forgotpass" class="forgetpassword forgetpassinline">Forget Password?</a>
+                    </div>
+
+                </form>
+            </div>
+            
+           
+        </div>
+       
+    </div>
+   </div>
