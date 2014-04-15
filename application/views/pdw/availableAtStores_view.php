d497002efc063db73b1ff3f9a90aa92eda5ea007
Rahul Sonar <sonar.soni@gmail.com>
2014-04-16 00:27:39 +0530

    html integration

--- application/views/pdw/availableAtStores_view.php
+++ application/views/pdw/availableAtStores_view.php
@@ -1,41 +1,41 @@
-<?php
-//print_debug($availableAtStores, __FILE__, __LINE__, 0);
-?>
 <div id="storeList">
-<div id="pdavil" class="subheadingholder">
-    <h2>AVAILABLE AT STORES (<?php echo count($availableAtStores); ?>):</h2>
-	<?php
-	$availableAtStoresIds=array();
-	foreach($availableAtStores as $store) {
-		$availableAtStoresIds[]=$store['storeId'];
-	}
-	?>
-    <span class="right"> 
-        <a href="#" class="savepdf" onclick="SavePDF(<?php echo $product['productId']; ?>); return false;">Save PDF</a>
-        <a href="#" class="savesearch" onclick="xajax_savesearch('<?php echo $product['productName']; ?>',<?php echo $product['productId']; ?>,<?php echo implode(',',$availableAtStoresIds); ?>); return false;">Save Search</a>
-    </span>
-</div>
-<div class="prodsubbox">
-    <div class="shortholder">
-        <div class="showing">
-            Showing: 
-            <select>
-                <option>1</option>
-                <option>2</option>
-            </select>
-            per page
-        </div>
-        <div class="sortright">
-            Sort By: 
-            <select name="sortStores" id="sortStores" onchange="javascript:sortAreas();">
-                <option <?php if(!empty($sorting['availability'])) { ?>selected="selected" <?php } ?> value="availability~availability">Availability</option>
-				<option <?php if(!empty($sorting['price']) && $sorting['price']=='asc') { ?>selected="selected" <?php } ?> value="price~asc">Price Low-High</option>
-				<option <?php if(!empty($sorting['price']) && $sorting['price']=='desc') { ?>selected="selected" <?php } ?> value="price~desc">Price High-Low</option>
-            </select>
-        </div>
-        <div class="sortright">
-            Filter Area: 
-			<?php
+<div class="space10"></div>
+	<div id="pdavil" class="subheadingholder">
+          
+    	<h2>AVAILABLE AT STORES (<?php echo count($availableAtStores); ?>):</h2>
+    	<?php
+			$availableAtStoresIds=array();
+			foreach($availableAtStores as $store) {
+				$availableAtStoresIds[]=$store['storeId'];
+			}
+		?>
+        <span class="right"> 
+			<a href="#" class="savepdf" onclick="SavePDF(<?php echo $product['productId']; ?>); return false;">Save PDF</a>
+	        <a href="#" class="savesearch" onclick="xajax_savesearch('<?php echo $product['productName']; ?>',<?php echo $product['productId']; ?>,<?php echo implode(',',$availableAtStoresIds); ?>); return false;">Save Search</a>
+		</span>
+    </div>
+            
+	<div class="prodsubbox">
+		<div class="shortholder">
+			<div class="showing">
+				Showing: 
+				<select>
+					<option>1</option>
+					<option>2</option>
+				</select>
+				per page
+			</div>
+			<div class="sortright">
+				Sort By: 
+				<select name="sortStores" id="sortStores" onchange="javascript:sortAreas();">
+	                <option <?php if(!empty($sorting['availability'])) { ?>selected="selected" <?php } ?> value="availability~availability">Availability</option>
+					<option <?php if(!empty($sorting['price']) && $sorting['price']=='asc') { ?>selected="selected" <?php } ?> value="price~asc">Price Low-High</option>
+					<option <?php if(!empty($sorting['price']) && $sorting['price']=='desc') { ?>selected="selected" <?php } ?> value="price~desc">Price High-Low</option>
+            	</select>
+			</div>
+			<div class="sortright">
+				Filter Area: 
+				<?php
 			$areaNames=$this->common_model->getSelectedAreaNames2($this->session->userdata('areasSelected'));
 			?>
             <select id="areaFilter" name="areaFilter" onchange="javascript:sortAreas();">
@@ -46,33 +46,29 @@
 				<?php }
 				?>
             </select>
-        </div>
-       <!-- <div class="sortright">
-            Sort By: 
-            <select>
-                <option>Price Low-High</option>
-            </select>
-        </div>-->
-    </div>
-
-    <?php foreach ($availableAtStores as $storeId => $storeArr) { ?>
-        <div class="proddbox">
-            <div class="proddboxcollitems">
-                <div class="frow row">
-                    <img src="<?php echo base_url(); ?>assets/<?php echo $this->config->item('themeCode'); ?>/images/prodthumb2.jpg" alt=""  />
-                </div>
-                <div class="srow row">
-                    <h2><?php echo $storeArr['storeName']; ?></h2>
-                    <p class="locationbor"><span class="locationbox"><?php echo $storeArr['areaName']; ?> ( 2 Km )</span>
-                        <span class="starholder">
+			</div>
+			
+		</div>
+<?php foreach ($availableAtStores as $storeId => $storeArr) { ?>
+		<div class="proddbox">
+			<div class="proddboxcollitems">
+			<div class="frow row">
+			<?php if(!empty($storeArr['storeLogo'])) { ?>
+                 <img src="<?php echo base_url().$this->config->item('storeLogoPath').$storeArr['storeLogo']; ?>" alt="<?php echo $storeArr['storeName']; ?>"  />
+                <?php } else { echo "&nbsp;"; } ?>
+				</div>
+                            <div class="srow row">
+				<h2><?php echo $storeArr['storeName']; ?></h2>
+				<p class="locationbor"><span class="locationbox"><?php echo $storeArr['areaName']; ?> ( 2 Km ) </span>
+				<span class="starholder">
+					<a href="#"><img src="<?php echo base_url(); ?>assets/<?php echo $this->config->item('themeCode'); ?>/images/star.jpg" alt=""></a>
                             <a href="#"><img src="<?php echo base_url(); ?>assets/<?php echo $this->config->item('themeCode'); ?>/images/star.jpg" alt=""></a>
                             <a href="#"><img src="<?php echo base_url(); ?>assets/<?php echo $this->config->item('themeCode'); ?>/images/star.jpg" alt=""></a>
                             <a href="#"><img src="<?php echo base_url(); ?>assets/<?php echo $this->config->item('themeCode'); ?>/images/star.jpg" alt=""></a>
                             <a href="#"><img src="<?php echo base_url(); ?>assets/<?php echo $this->config->item('themeCode'); ?>/images/star.jpg" alt=""></a>
-                            <a href="#"><img src="<?php echo base_url(); ?>assets/<?php echo $this->config->item('themeCode'); ?>/images/star.jpg" alt=""></a>
-                        </span>
-                    </p>
-                            <label>
+				</span>
+				</p>
+                                <label>
                                     <span class="left check">
                                         <input type="checkbox" id="sto_compare"/>
                                     </span>
@@ -80,81 +76,18 @@
                                         Add To Store Compare
                                     </span>
                                 </label>
+				
+			</div>
+                            <div class="trow row">
+			
+                            <span class="inrbox">
+						INR <?php echo $storeArr['sellPrice']; ?>
+					</span>
 
-                </div>
-                <div class="trow row">
-                    <span class="inrbox">
-                        INR <?php echo $storeArr['sellPrice']; ?>
-                    </span>
-                    <div class="space10"></div>
-
-                </div>
-                <div class="trow row">
-                    <ul class="listorange">
-						<?php if($storeArr['offerPrice']>0) { ?>
-                        <li><a href="#">Offer available</a></li>
-						<?php } ?>
-                        <li><a href="#">Last <?php echo $storeArr['qty']; ?> piece<?php if($storeArr['qty']>1) { echo 's'; } ?> left</a></li>
-                    </ul>
-                </div>
-
-                </div>
-
-            <a href="javascript:void(0)" class="prodsubarrowd"></a>
-            <a href="javascript:void(0)" class="prodsubarrow"></a>
-
-            <div class="pdetailsexpand">
-                <div class="srow row">
-
-                    <table class="addresstab">
-                        <tr>
-                            <td class="adddefault">
-                                <p><strong>Address:</strong> <br />
-                                    2/102, Floor 2 <br />
-                                    Inorbit Mall, Nagar Road <br />
-                                    Viman Nagar, Pune- 411010
-                                </p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <p><strong>Contact Number:</strong> <br />
-                                    Mr. K.P. Roy </p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <p><strong>Contact Person:</strong> <br />
-                                    +91 12345 2345 <br /> 022 12345788</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <p><strong>Store Timings:</strong> <br />
-                                    9:00 AM to 6:00 PM<br /> Monday - Saturday</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="emailsmsbox">
-                                <a href="#" class="sendsms">Send on SMS</a>
-                                <a href="#" class="sendsms1">Send on Email</a>
-                            </td>
-                        </tr>
-                    </table>
-
-                </div>
-                <div class="trow row">
-                    <ul class="listorange">
-                        <li><a href="#">12 Store Reviews</a></li>
-                    </ul>
-                </div>
-                <div class="frrow row">
-
-                    <div class="space10"></div>
-                    <table class="storebox">
-                        <tr>
-                            <td>
-                            <?php if ($this->session->userdata('interfaceUsername') == '') {
+			</div>
+			<div class="frrow row">
+			
+			 <?php if ($this->session->userdata('interfaceUsername') == '') {
                             	$buyNowfunc="shortLogin('buyNow','".$storeId."');";
                             	$targetBox='#shotlogin';
                             } 
@@ -163,51 +96,141 @@
 								$targetBox='#bargainreq';
 							}
 							?>
-                                <a href="<?php echo $targetBox; ?>" id="buyNow_<?php echo $storeId; ?>" class="btncomman rounded btnshadow">BUY NOW</a>
-								<a href="<?php echo $targetBox; ?>" class="btncomman rounded btnshadow bargainBtn">BARGAIN</a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="storeboxtd">
-                                <label><input type="checkbox" /> Add to STORE COMPARE</label> 
-                                <label><input type="checkbox" /> Add to STORE COMPARE</label>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div class="giftbox">
-                                    <a href="#">
-                                        <span><img src="<?php echo base_url(); ?>assets/<?php echo $this->config->item('themeCode'); ?>/images/cgift.png" alt="" /></span>
-                                        Offer <br /> Available
-                                    </a>
-                                    <a href="#">
-                                        <span><img src="<?php echo base_url(); ?>assets/<?php echo $this->config->item('themeCode'); ?>/images/ccar.png" alt="" /></span>
-                                        Parking <br /> Available
-                                    </a>
-                                    <a href="#">
-                                        <span><img src="<?php echo base_url(); ?>assets/<?php echo $this->config->item('themeCode'); ?>/images/ccard.png" alt="" /></span>
-                                        Credit/Debit <br /> Card Accepted
-                                    </a>
-                                    <a href="#">
-                                        <span><img src="<?php echo base_url(); ?>assets/<?php echo $this->config->item('themeCode'); ?>/images/crup.png" alt="" /></span>
-                                        Cash <br /> Accepted
-                                    </a>
-                                </div>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
+					<a href="<?php echo $targetBox; ?>" id="buyNow_<?php echo $storeId; ?>" class="btncomman rounded btnshadow">BUY NOW</a> 
+					<a href="<?php echo $targetBox; ?>" class="btncomman rounded btnshadow bargain_req inline">BARGAIN</a>
+					<ul class="listorange">
+					<li><a href="#">12 Store Reviews</a></li>
+					<?php if($storeArr['offerPrice']>0) { ?>
+                        <li><a href="#">Offer available</a></li>
+						<?php } ?>
+                                        </ul>
+                        </div>
+			</div>
 
+			<a href="javascript:void(0)" class="prodsubarrowd"></a>
+			<a href="javascript:void(0)" class="prodsubarrow"></a>
+			
+			<div class="pdetailsexpand">
+				<div class="srow row">
+				
+				<table class="addresstab">
+				<?php if(!empty($storeArr['address'])) {?>
+					<tr>
+						<td class="adddefault">
+						
+							<p><strong>Address:</strong> <br />
+							
+							<?php echo $storeArr['address']; ?> - <?php echo $storeArr['pincode']; ?>
+							
+							
+							</p>
+						</td>
+					</tr>
+					<?php } ?>
+					<?php if(!empty($storeArr['contactPerson'])) {?>
+					<tr>
+						<td>
+							<p><strong>Contact Person:</strong> <br />
+							<?php echo $storeArr['contactPerson']; ?></p>
+						</td>
+					</tr>
+					<?php } ?>
+					<?php if(!empty($storeArr['mobile']) || !empty($storeArr['phone'])) { ?>
+					<tr>
+						<td>
+							<p><strong>Contact Number:</strong> <br />
+							<?php echo $storeArr['mobile']; ?><br /> <?php echo $storeArr['phone']; ?></p>
+						</td>
+					</tr>
+					<?php } ?>
+					<?php if(!empty($storeArr['storeTimings'])) {?>
+					<tr>
+						<td>
+							<p><strong>Store Timings:</strong> <br />
+							<?php echo $storeArr['storeTimings']; ?></p>
+						</td>
+					</tr>
+					<?php } ?>
+					<tr>
+						<td class="emailsmsbox">
+							<a href="#" class="sendsms">Send on SMS</a>
+							<a href="#" class="sendsms1">Send on Email</a>
+						</td>
+					</tr>
+				</table>
+				
+			</div>
+			<table class="storereviewtable">
+				<tr>
+					<td></td>
+					<td>
+					<table class="storebox">
+						
+						<tr>
+							<td>
+								<div class="giftbox">
+								<?php if($storeArr['offerPrice']>0) { ?>
+								<a href="#">
+									<span><img src="<?php echo base_url(); ?>assets/<?php echo $this->config->item('themeCode'); ?>/images/cgift.png" alt="" /></span>
+									Offer <br /> Available
+								</a>
+								<?php } ?>
+								<?php if($storeArr['isParking']>0) { ?>
+								<a href="#">
+									<span><img src="<?php echo base_url(); ?>assets/<?php echo $this->config->item('themeCode'); ?>/images/ccar.png" alt="" /></span>
+									Parking <br /> Available
+								</a>
+								<?php } ?>
+								<?php
+								$paymentMethods=explode(",",$storeArr['paymentMethods']); 
+								if(in_array('card',$paymentMethods)) { ?>
+								<a href="#">
+									<span><img src="<?php echo base_url(); ?>assets/<?php echo $this->config->item('themeCode'); ?>/images/ccard.png" alt="" /></span>
+									Credit/Debit <br /> Card Accepted
+								</a>
+								<?php } ?>
+								<?php if(in_array('cash',$paymentMethods)) { ?>
+								<a href="#">
+									<span><img src="<?php echo base_url(); ?>assets/<?php echo $this->config->item('themeCode'); ?>/images/crup.png" alt="" /></span>
+									Cash <br /> Accepted
+								</a>
+								<?php } ?>
+								</div>
+							</td>
+						</tr>
+					</table>
+						
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+                                            <div class="space50"></div>
+                                            <span class="left"><h2>Store Review</h2></span>	
+                                                <span class="right"><a href="#" class="showalllink ">Show All Review</a></span>
+                                          
+						<div class="space10"></div>
+                                                <p><strong>Amit Jain:</strong> <span id="review_line">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</span></p>	
+						<a href="javascript:click()" id='fullreview' class="reviewlink">Read Full Review</a>	
+						<!--<a href="#" class="showalllink">Show All Review</a>-->			
+					</td>
+				</tr>
+			</table>
+			
+		
+			</div>
+			
+			</div>
+<?php } ?>
+
+	</div>
+	
+	<a name="#anchor2"></a>
+
+        <div class="inrbox loadmorestore">
+            Show more store
         </div>
-    <?php } ?>
-	<?php if($showloadMore) { ?>
-	<div id='loadMoreItems'>
-                <a href="javascript:void(0);" class="loadmore loadMoreItems">Load More Stores</a>
-            </div>
-		<?php } ?>
+
 </div>
-<div class="space15"></div>
 <script>
 	function SavePDF(product_id) {
 		window.open('<?php echo site_url('product/pdf/'); ?>/'+product_id);
@@ -220,10 +243,9 @@
 	 
 	$(function(){
 	
-	$('.loadMoreItems').click(function(){
+	$('.loadmorestore').click(function(){
 		xajax_loadmorestores(<?php echo $product['productId']; ?>,<?php echo $availableAtStoresTotal; ?>);
 	});
 	});
 	
 </script>
-</div>
